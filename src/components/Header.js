d2e0c476c02ac0f90c6ed9347c187d4acfb1df65
Zefeng <imnot.zefeng@gmail.com>
2023-05-27 08:36:07 +0800

    updated css, pushing to deploy and check on mobile browser

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ function Header() {
           <div className="header-content flex flex-center text-center text-white" >
             <h2 className="header-title"> Cocktail receipes you love, right here.</h2> <br/>
             <p className="header-text fontsize-18 fontwidth-3" >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <SearchForm type="text" placeholder="Search for a Cocktail..."/>
+            <SearchForm type="text" placeholder="Search for a Cocktail..."/> <br/>
           </div>
       </header>
 
